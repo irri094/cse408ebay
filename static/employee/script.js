bf7c5a20b3beb19743d54d98768dec200e7f7ed0
Zahin112 <109434296+Zahin112@users.noreply.github.com>
2022-08-28 20:25:23 +0600

    summary card

--- static/employee/script.js
+++ static/employee/script.js
@@ -2,13 +2,14 @@ $('#btn-accept').click(function () {
     console.log("accept confirmation button pressed")
 
     let o_name = document.getElementById('ord_id').innerText
+    let acpt = this
     
     // let inv_id = $("#auction_product_id").val()
     mydata = {
         name: o_name,
     }
 
-    console.log()
+    console.log(acpt)
 
     $.ajax(
         {
@@ -24,6 +25,8 @@ $('#btn-accept').click(function () {
                         'success'
                     )
 
+                    $(acpt).closest("tr").fadeOut()
+
                 }
                 else{
                     Swal.fire({
@@ -37,6 +40,82 @@ $('#btn-accept').click(function () {
 })
 
 $('#btn-reject').click(function () {
+    console.log("reject confirmation button pressed")
+
+    let o_name = document.getElementById('ord_id').innerText
+    
+    // let inv_id = $("#auction_product_id").val()
+    mydata = {
+        name: o_name,
+    }
+
+    console.log()
+
+    $.ajax(
+        {
+            url: "reject/",
+            method: 'GET',
+            data: mydata,
+            success: function (data) {
+                if (data.status == 1) {
+
+                    Swal.fire(
+                        'Order Rejected!',
+                        'Order returned to Seller',
+                        'success'
+                    )
+
+                }
+                else{
+                    Swal.fire({
+                        icon: 'error',
+                        text: 'start time and end time are incompatible',
+                    })
+                }
+            }
+        }
+    )
+})
+
+$('#btn-accept-all').click(function () {
+    console.log("reject confirmation button pressed")
+
+    let o_name = document.getElementById('ord_id').innerText
+    
+    // let inv_id = $("#auction_product_id").val()
+    mydata = {
+        name: o_name,
+    }
+
+    console.log()
+
+    $.ajax(
+        {
+            url: "reject/",
+            method: 'GET',
+            data: mydata,
+            success: function (data) {
+                if (data.status == 1) {
+
+                    Swal.fire(
+                        'Order Rejected!',
+                        'Order returned to Seller',
+                        'success'
+                    )
+
+                }
+                else{
+                    Swal.fire({
+                        icon: 'error',
+                        text: 'start time and end time are incompatible',
+                    })
+                }
+            }
+        }
+    )
+})
+
+$('#btn-reject-all').click(function () {
     console.log("reject confirmation button pressed")
 
     let o_name = document.getElementById('ord_id').innerText
